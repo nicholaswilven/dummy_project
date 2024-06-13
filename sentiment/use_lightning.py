127c74ae87f7679a6c6f382b3302fd167b2da2a4
nicholaswilven <hikari.netto.355@gmail.com>
2024-06-13 23:03:02 +0700

    gas

--- sentiment/use_lightning.py
+++ sentiment/use_lightning.py
@@ -1,7 +1,8 @@
 from lightning import LightningModule, LightningDataModule, Trainer
 from datasets import load_dataset, concatenate_datasets
+from torch.optim.lr_scheduler import LambdaLR
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from torch.utils.data import Dataset, DataLoader, random_split, default_collate
+from torch.utils.data import DataLoader, random_split, default_collate
 from torch import nn, optim
 from lightning.pytorch.callbacks import ModelCheckpoint
 import torch
@@ -17,6 +18,17 @@ from huggingface_hub import login
 login(os.getenv("ACCESS_TOKEN"))
 
 NUM_WORKERS = 64
+BATCH_SIZE=128
+EPOCH=3
+WEIGHT_DECAY=0.01
+VAL_SIZE=0.05
+LEARNING_RATE=0.00001
+BASE_MODEL_NAME="awidjaja/pretrained-xlmR-food"
+EN_DATASET="carant-ai/english_sentiment_dataset"
+ID_DATASET="carant-ai/indonesian_sentiment_dataset"
+PJRT_DEVICE="TPU"
+ACCELERATOR="tpu"
+HUB_MODEL_NAME="awidjaja/sentiment-xlmR-base"
 
 def collator(batch):
     keys = batch[0].keys()
@@ -49,7 +61,7 @@ def tokenize(dataset, tokenizer, label_index):
     )
 
 class Model(LightningModule):
-    def __init__(self, model_name: str = "xlm-roberta-base", label_index: dict = {}):
+    def __init__(self, model_name: str = "xlm-roberta-base", label_index: dict = {} , total_steps = 0, warmup_steps = 0):
         super().__init__()
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_name,
@@ -57,6 +69,8 @@ class Model(LightningModule):
             label2id = label_index,
             id2label = {v:k for k,v in label_index.items()}
         )
+        self.total_steps = total_steps
+        self.warmup_steps = warmup_steps
         self.save_hyperparameters()
         self.loss_fn = nn.CrossEntropyLoss()
 
@@ -81,31 +95,37 @@ class Model(LightningModule):
         return loss
 
     def configure_optimizers(self):
-        weight_decay = 0.01
         param_optimizer = list(self.named_parameters())
-
-        # Remove LayerNorm from weight decay params
         no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
             {
                 "params": [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-                "weight_decay": weight_decay,
+                "weight_decay": WEIGHT_DECAY,
             },
             {"params": [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
         ]
-
-        optimizer = optim.AdamW(optimizer_grouped_parameters, lr=float(os.getenv("LEARNING_RATE")))
-        return optimizer
+        optimizer = optim.AdamW(optimizer_grouped_parameters, lr=LEARNING_RATE)
+        # Define warmup scheduler
+        def warmup_scheduler(step):
+            return min(1.0, step / self.warmup_steps)
+        
+        # Define linear decay scheduler
+        def linear_decay_scheduler(step):
+            return max(0.0, 1.0 - step / self.total_steps)
+        
+        scheduler = LambdaLR(optimizer, lr_lambda=lambda step: warmup_scheduler(step) * linear_decay_scheduler(step))
+        
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
 
 
 class Sentence(LightningDataModule):
     def __init__(self, model_name: str = "xlm-roberta-base", val_ratio = 0.05, label_index = {}):
         super().__init__()
         id_dataset = load_dataset(
-            os.getenv("ID_DATASET"), split = "train",
+            ID_DATASET, split = "train",
         )
         en_dataset = load_dataset(
-            os.getenv("EN_DATASET"), split = "train",
+            EN_DATASET, split = "train",
         )
         concat_ds = concatenate_datasets([id_dataset, en_dataset]).shuffle(seed=42)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -122,10 +142,10 @@ class Sentence(LightningDataModule):
         pass
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size = int(os.getenv("BATCH_SIZE")), collate_fn = collator, num_workers = NUM_WORKERS)
+        return DataLoader(self.train_dataset, batch_size = BATCH_SIZE, collate_fn = collator, num_workers = NUM_WORKERS)
     
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size = int(os.getenv("BATCH_SIZE")), collate_fn = collator, num_workers = NUM_WORKERS)
+        return DataLoader(self.val_dataset, batch_size = BATCH_SIZE, collate_fn = collator, num_workers = NUM_WORKERS)
 
 def main():
     label_index = {
@@ -133,31 +153,37 @@ def main():
         "neutral": 1,
         "positive": 2
         }
-    model = Model(model_name = os.getenv("BASE_MODEL_NAME"), label_index = label_index)
-    data = Sentence(model_name = os.getenv("BASE_MODEL_NAME"), val_ratio = float(os.getenv("VAL_SIZE")), label_index = label_index)
+    data = Sentence(model_name = BASE_MODEL_NAME, val_ratio = VAL_SIZE, label_index = label_index)
+    TOTAL_STEPS = EPOCH*len(data.train_dataloader())
+    WARMUP_STEPS = int(0.1*TOTAL_STEPS)
+    model = Model(model_name = BASE_MODEL_NAME, label_index = label_index, total_steps = TOTAL_STEPS, warmup_steps = WARMUP_STEPS)
     checkpoint_callback = ModelCheckpoint(monitor = 'val_loss')
-    
     wandblogger = WandbLogger(
         log_model = True,
         mode = "online",
         project = "intial-sentiment",
         config = {
-            "learning_rate": os.getenv("LEARNING_RATE"),
-            "weight_decay": 0.01,
-            "epochs": os.getenv("EPOCH"),
-            "batch_size": os.getenv("BATCH_SIZE"),
+            "learning_rate": LEARNING_RATE,
+            "weight_decay": WEIGHT_DECAY,
+            "epochs": EPOCH,
+            "batch_size": BATCH_SIZE,
             "block_size": 512,
-            "base_model_name": os.getenv("BASE_MODEL_NAME"),
-            "hub_model_name": os.getenv("HUB_MODEL_NAME"),
-            "dataset_name": os.getenv("ID_DATASET") +","+os.getenv("EN_DATASET")
+            "base_model_name": BASE_MODEL_NAME,
+            "hub_model_name": HUB_MODEL_NAME,
+            "dataset_name": ID_DATASET +","+EN_DATASET
             }
         )
     
-    trainer = Trainer(accelerator = os.getenv("ACCELERATOR"), max_epochs = int(os.getenv("EPOCH")), callbacks =  [checkpoint_callback])
+    trainer = Trainer(
+        accelerator = ACCELERATOR,
+        max_epochs = EPOCH,
+        callbacks =  [checkpoint_callback],
+        logger = wandblogger
+        )
     trainer.fit(model, data)
-    m = Model.load_from_checkpoint(checkpoint_callback.best_model_path)
-    m.model.push_to_hub(os.getenv("HUB_MODEL_NAME"))
-    data.tokenizer.push_to_hub(os.getenv("HUB_MODEL_NAME"))
+    
+    model.model.push_to_hub(HUB_MODEL_NAME, private = True)
+    data.tokenizer.push_to_hub(HUB_MODEL_NAME, private = True)
 
 if __name__ == "__main__":
     main()
